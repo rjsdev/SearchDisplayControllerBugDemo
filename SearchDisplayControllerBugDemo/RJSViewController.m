b3167084f72eba25e717c424c03c5c0f1c2be76c
Robert Stainsby <rjsdev@mac.com>
2013-10-02 12:38:25 +1000

    Add a toolbar at bottom to illustrate colour difference

--- SearchDisplayControllerBugDemo/RJSViewController.m
+++ SearchDisplayControllerBugDemo/RJSViewController.m
@@ -11,6 +11,7 @@
 @interface RJSViewController () <UISearchBarDelegate>
 
 @property (nonatomic, strong) UISearchBar * searchBar;
+@property (nonatomic, strong) UIToolbar * toolbar;
 @property (nonatomic, strong) UISearchDisplayController * mySearchDisplayController;
 
 @end
@@ -25,6 +26,9 @@
     self.searchBar.delegate = self;
     [self.view addSubview:self.searchBar];
     
+    self.toolbar = [[UIToolbar alloc] initWithFrame:CGRectZero];
+    [self.view addSubview:self.toolbar];
+    
     // Comment out the following statement to extend search bar under status bar, as expected
     self.mySearchDisplayController
     = [[UISearchDisplayController alloc] initWithSearchBar:self.searchBar
@@ -34,16 +38,25 @@
 
 -(void)viewDidLayoutSubviews
 {
+    const CGFloat kDefaultStatusBarHeight = 20.0;
+    const CGFloat kBarHeight = 44.0;
+    
     self.searchBar.frame = CGRectMake(0.0,
-                                      20.0,
+                                      kDefaultStatusBarHeight,
                                       CGRectGetWidth(self.view.bounds),
-                                      44.0
+                                      kBarHeight
                                       );
+
+    self.toolbar.frame = CGRectMake(0.0,
+                                    CGRectGetHeight(self.view.bounds) - kBarHeight,
+                                    CGRectGetWidth(self.view.bounds),
+                                    kBarHeight
+                                    );
 }
 
 - (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar
 {
-    UIBarPosition result = UIBarPositionTopAttached;
+    UIBarPosition result = UIBarPositionAny;
     
     if ( bar == self.searchBar )
     {
